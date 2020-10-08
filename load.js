a70fb880026cce50287c0cce53a3b795e2849c62
ipanghu <56402715+bypanghu@users.noreply.github.com>
2020-10-08 14:35:59 +0000

    Update load.js

--- load.js
+++ load.js
@@ -1,8 +1,8 @@
 try {
-    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: 'https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
-    $.ajax({url: 'https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
     live2d_settings['modelId'] = 1;                  // 默认模型 ID
@@ -10,5 +10,5 @@ try {
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
     live2d_settings['homePageUrl'] = 'https://bypanghu.github.io/blog/';        // 主页地址，可选 'auto'(自动), '{URL 网址}'
     /* 在 initModel 前添加 */
-    initModel('https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/assets/waifu-tips.json');
+    initModel('https://cdn.jsdelivr.net/gh/bypanghu/kanbanniang@master/waifu-tips.json');
 } catch(err) { console.log('[Error] JQuery is not defined.') }
